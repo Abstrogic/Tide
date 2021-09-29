690a766d5dcef61481ec5c3756320e405a17f130
Tim Zhao <timzhao1212@163.com>
2021-09-30 07:41:23 +0800

    changed structure

--- Ryte.py
+++ Ryte.py
@@ -1,21 +1,21 @@
 import py_cui
 import os
 
-class FileSelectionList:
-
-    def __init__(self, base):
-        FilesListMenu = base.add_scroll_menu(title="Files in Current Directory", row=0, column=0, row_span=3, column_span=3, padx=1, pady=0)
-        FilesListMenu.add_item_list(os.listdir())
-
 class Ryte:
 
     def __init__(self, base):
         self.base = base
-        self.main()
 
-    def main(self):
-        FileSelectionList(self.base)
+        self.FilesListMenu = base.add_scroll_menu(title="Files in Current Directory", row=0, column=0, row_span=2, column_span=2, padx=1, pady=0)
+        self.FilesListMenu.add_item_list(os.listdir())
 
-base = py_cui.PyCUI(3, 3)
+        self.UserInputBox = base.add_text_box(title="Enter File to Edit", row=3, column=3, row_span = 1, column_span = 1, padx = 1, pady = 0, initial_text="")
+        
+    def OpenFile(self):
+        file = self.UserInputBox.get()
+
+
+base = py_cui.PyCUI(4, 4)
+base.set_title("Ryte Text Editor")
 app = Ryte(base)
 base.start()
