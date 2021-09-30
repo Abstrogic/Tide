0e5537ad0ff398f2a3d557c3ddd842152194cb8d
Tim Zhao <timzhao1212@163.com>
2021-10-01 07:36:44 +0800

    Many Improvements

--- Ryte.py
+++ Ryte.py
@@ -1,3 +1,4 @@
+from genericpath import isdir, isfile
 import py_cui
 import os
 
@@ -6,16 +7,41 @@ class Ryte:
     def __init__(self, base):
         self.base = base
 
-        self.FilesListMenu = base.add_scroll_menu(title="Files in Current Directory", row=0, column=0, row_span=2, column_span=2, padx=1, pady=0)
+        # Menu that lists files in current directory, displayed on the left of Ryte GUI
+        self.FilesListMenu = self.base.add_scroll_menu(title="Select File to Edit", row=0, column=0, row_span=3, column_span=2, padx=1, pady=0)
         self.FilesListMenu.add_item_list(os.listdir())
+        self.FilesListMenu.add_key_command(py_cui.keys.KEY_ENTER, self.OpenDirOrFile)
 
-        self.UserInputBox = base.add_text_box(title="Enter File to Edit", row=3, column=3, row_span = 1, column_span = 1, padx = 1, pady = 0, initial_text="")
-        
-    def OpenFile(self):
-        file = self.UserInputBox.get()
+        # Editing Box
+        self.EditBox = self.base.add_text_block(title="Editor", row=0, column=2, row_span=4, column_span=3, padx=1, pady=0, initial_text="")
+
+        # Ryte Status Box (To catch and display raised exceptions)
+        self.StatusBox = self.base.add_text_box(title="Ryte Status", row=3, column=0, row_span=1, column_span=2, padx=1, pady=0, initial_text="")
+
+    def OpenDirOrFile(self):
+
+        # Get the absolute path of the selected file or folder
+        cwd = os.getcwd()
+        name = self.FilesListMenu.get()
+        path = os.path.join(cwd, name)
+
+        if(os.path.exists(path)):
+            if(os.path.isfile(path)):
+                OpenFile = open(path, "r")
+                FileData = OpenFile.read()
+                OpenFile.close()
+
+                self.EditBox.set_text(FileData)
+                self.EditBox.set_title("Editing File: " + path)
+                
+
+            if(os.path.isdir(path)):
+                os.chdir(path)
+                data = os.listdir()
+                self.FilesListMenu.add_item_list(data)
 
 
-base = py_cui.PyCUI(4, 4)
+base = py_cui.PyCUI(num_rows=4, num_cols=5)
 base.set_title("Ryte Text Editor")
 app = Ryte(base)
 base.start()
