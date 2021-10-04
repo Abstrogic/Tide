a1fa4fe87477be97658c03549260885ecf251e23
Tim Zhao <timzhao1212@163.com>
2021-10-04 08:45:24 +0800

    Added basic color

--- tide.py
+++ tide.py
@@ -1,4 +1,5 @@
 from genericpath import isdir, isfile
+import traceback
 import py_cui
 import os
 
@@ -6,23 +7,29 @@ class Tide:
 
     def __init__(self, base):
         self.base = base
+        self.base.set_title("Tide: Terminal Integrated Development Enviroment")
 
         # Menu that lists all of the Tide Operations
-        self.OperationsMenu = self.base.add_scroll_menu(title="Tide Operations", row=0, column=0, row_span=2, column_span=2, padx=1, pady=0)
         self.commands = ["Open File (CTRL-o)", "Locate File (CTRL-l)"]
+        self.OperationsMenu = self.base.add_scroll_menu(title="Tide Operations", row=0, column=0, row_span=2, column_span=2, padx=1, pady=0)
         self.OperationsMenu.add_item_list(self.commands)
+        self.OperationsMenu.set_color(py_cui.CYAN_ON_BLACK)
         
 
         # Menu that lists files in current directory, displayed on the left of Tide GUI
         self.DirNav = self.base.add_scroll_menu(title="Directory Navigation", row=2, column=0, row_span=2, column_span=2, padx=1, pady=0)
         self.DirNav.add_item_list(os.listdir())
         self.DirNav.add_key_command(py_cui.keys.KEY_ENTER, self.OpenFileOrDir)
+        self.DirNav.add_key_command(py_cui.keys.KEY_BACKSPACE, self.ChangeToParentDir)
+        self.DirNav.set_color(py_cui.CYAN_ON_BLACK)
 
         # Tide Status Box (To catch and display raised exceptions)
         self.StatusBox = self.base.add_text_block(title="Tide Status", row=4, column=0, row_span=1, column_span=5, padx=1, pady=0, initial_text="")
+        self.StatusBox.set_color(py_cui.CYAN_ON_BLACK)
 
         # Editing Box
         self.EditBox = self.base.add_text_block(title="Editor", row=0, column=2, row_span=4, column_span=3, padx=1, pady=0, initial_text="")
+        self.EditBox.set_color(py_cui.CYAN_ON_BLACK)
 
     def OpenFileOrDir(self):
         try: 
@@ -52,15 +59,28 @@ class Tide:
         except Exception:
             self.UpdateStatus(Exception)
 
-    def UpdateStatus(self, UpdateContent):
-        self.StatusBox.clear()
+    def ChangeToParentDir(self):
         try:
+            ParentDir = os.path.dirname(os.getcwd())
+            ParentDirAbsPath = os.path.abspath(ParentDir)
+            if(os.path.isdir(ParentDirAbsPath)):
+                os.chdir(ParentDirAbsPath)
+                files = os.listdir()
+                self.DirNav.clear()
+                self.DirNav.add_item_list(files)
+
+        except Exception:
+            self.UpdateStatus(Exception)
+
+    def UpdateStatus(self, UpdateContent):
+        try:
+            self.StatusBox.clear()
             self.StatusBox.set_text(UpdateContent)
 
         except Exception:
-            self.StatusBox.set_text("Status Update Error: " + Exception)
+            error = traceback.format_exc()
+            self.StatusBox.set_text("Error: " + error)
 
 base = py_cui.PyCUI(num_rows=5, num_cols=5)
-base.set_title("Tide: Terminal Integrated Development Enviroment")
 app = Tide(base)
 base.start()
