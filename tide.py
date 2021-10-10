3f002825c9d05ddc6f47f802324b0a831ccddf13
Tim Zhao <timzhao1212@163.com>
2021-10-11 07:34:49 +0800

    working on command line argument parsing

--- tide.py
+++ tide.py
@@ -2,22 +2,30 @@ from genericpath import isdir, isfile
 import traceback
 import py_cui
 import os
+import argparse
 
 class Tide:
 
-    def __init__(self, base):
+    def __init__(self, base, startdir):
+
+        try:
+            os.chdir(dir)
+
+        except Exception:
+            self.UpdateStatus(Exception)
+
         self.base = base
         self.base.set_title("Tide: Terminal Integrated Development Enviroment")
 
         # Menu that lists all of the Tide Operations
-        self.operations = ["Save File (CTRL-s)", "Create New File (CTRL-n)", "Refresh Editor (CTRL-r)", "Delete File (CTRL-d)"]
-        self.OperationsMenu = self.base.add_scroll_menu(title="Tide Operations List", row=0, column=0, row_span=2, column_span=2, padx=1, pady=0)
+        self.operations = ["Save File (CTRL-s)", "Create New File (CTRL-n)", "Refresh Editor (CTRL-r)", "Delete File (CTRL-d)", "Quick Directory Change (CTRL-q)"]
+        self.OperationsMenu = self.base.add_scroll_menu(title="Tide Operations List", row=0, column=0, row_span=1, column_span=2, padx=1, pady=0)
         self.OperationsMenu.add_item_list(self.operations)
         self.OperationsMenu.set_color(py_cui.CYAN_ON_BLACK)
         self.OperationsMenu.add_key_command(py_cui.keys.KEY_ENTER, self.OperationsMenuFunction)
 
         # Menu that lists files in current directory, displayed on the left of Tide GUI
-        self.DirNav = self.base.add_scroll_menu(title="Directory Navigation", row=2, column=0, row_span=2, column_span=2, padx=1, pady=0)
+        self.DirNav = self.base.add_scroll_menu(title="Directory Navigation", row=1, column=0, row_span=5, column_span=2, padx=1, pady=0)
         self.DirNav.add_item_list(os.listdir())
         self.DirNav.set_color(py_cui.CYAN_ON_BLACK)
         self.DirNav.add_key_command(py_cui.keys.KEY_ENTER, self.OpenFileOrDir)
@@ -26,11 +34,11 @@ class Tide:
 
 
         # Tide Status Box (To catch and display raised exceptions)
-        self.StatusBox = self.base.add_text_block(title="Tide Status", row=4, column=0, row_span=1, column_span=10, padx=1, pady=0, initial_text="")
+        self.StatusBox = self.base.add_text_block(title="Tide Status", row=6, column=0, row_span=1, column_span=10, padx=1, pady=0, initial_text="")
         self.StatusBox.set_color(py_cui.GREEN_ON_BLACK)
 
         # Editing Box
-        self.EditBox = self.base.add_text_block(title="Editor", row=0, column=2, row_span=4, column_span=8, padx=1, pady=0, initial_text="")
+        self.EditBox = self.base.add_text_block(title="Editor", row=0, column=2, row_span=6, column_span=8, padx=1, pady=0, initial_text="")
         self.EditBox.set_color(py_cui.CYAN_ON_BLACK)
         self.EditBox.add_key_command(py_cui.keys.KEY_CTRL_S, self.SaveOpenFile)
         self.EditBox.add_key_command(py_cui.keys.KEY_CTRL_N, self.CreateNewFilePrompt)
@@ -54,6 +62,10 @@ class Tide:
                     # Refresh Edit Box
                     if(SelectedOperation == self.operations[2]):
                         self.RefreshEditBox()
+
+                    # Delete File
+                    if(SelectedOperation == self.operations[3]):
+                        self.DeleteFile()
 
         except Exception:
             self.UpdateStatus(Exception)
@@ -87,8 +99,21 @@ class Tide:
             file = self.DirNav.get()
             path = os.path.abspath(file)
 
+            def Delete(choice):
+                if(choice == True):
+                    os.remove(path)
+                    self.ShowMsgPopup("File Deleted.", "This file has been deleted: " + path)
+                    self.UpdateStatus("File deleted: " + path)
+                    self.DirNav.clear()
+                    self.DirNav.add_item_list(os.listdir())
+                    self.RefreshEditBox()
+
+                if(choice == False):
+                    self.ShowMsgPopup("File Not Deleted.", "The file deletion operation has been cancelled.")
+                    self.UpdateStatus("File Not Deleted. The file deletion operation has been cancelled.")
+
             if(os.path.exists(path)):
-                os.remove(path)
+                self.base.show_yes_no_popup("Confirm deletion of file: " + path, Delete)
 
         except Exception:
             self.UpdateStatus(Exception)
@@ -100,31 +125,28 @@ class Tide:
                 file = open(self.EditBox.get_title(), "w")
                 file.write(self.EditBox.get())
                 file.close()
-                self.ShowMsgPopup("File Saved", "The file has been saved as: " + self.EditBox.get_title())
+                self.ShowMsgPopup("File Saved.", "The file has been saved as: " + self.EditBox.get_title())
                 self.UpdateStatus("File Saved As: " + self.EditBox.get_title())
             
             else:
-                self.ShowWarningPopup("Unable to save file", "There is no file open in the editor.")
+                self.ShowWarningPopup("Unable to save file.", "There is no file open in the editor.")
                 self.UpdateStatus("Unable to save file. There isn't a file open in the editor.")
-
-        except Exception:
-            self.UpdateStatus(Exception)
-
-    def CreateNewFile(self, file):
-        try:
-            f = open(file, "x")
-            f.write(self.EditBox.get())
-            f.close()
-            self.DirNav.clear()
-            self.DirNav.add_item_list(os.listdir())
 
         except Exception:
             self.UpdateStatus(Exception)
 
     def CreateNewFilePrompt(self):
         try:
+            def CreateNewFile(file):
+                f = open(file, "x")
+                f.write(self.EditBox.get())
+                f.close()
+                self.DirNav.clear()
+                self.DirNav.add_item_list(os.listdir())
+                self.UpdateStatus("New file created: " + os.path.abspath(file))
+
             if(self.EditBox.get_title() == "Editor"):
-                self.base.show_text_box_popup("File to save name: ", self.CreateNewFile)
+                self.base.show_text_box_popup("File to save name: ", CreateNewFile)
 
         except Exception:
             self.UpdateStatus(Exception)
@@ -190,6 +212,15 @@ class Tide:
             self.StatusBox.set_text("Error: " + error)
             self.StatusBox.set_color(py_cui.RED_ON_BLACK)
 
-base = py_cui.PyCUI(num_rows=5, num_cols=10)
-app = Tide(base)
+def ParseCMDArgs():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-startdir", help="Directory to start Tide in.", type=str)
+    args = vars(parser.parse_args())
+
+    print(args)
+
+ParseCMDArgs()
+
+base = py_cui.PyCUI(num_rows=7, num_cols=10)
+app = Tide(base, startdir="D:\Stuff\PythonProjects\Tide\py_cui-docs")
 base.start()
